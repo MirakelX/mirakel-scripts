2e4bc153f6c5abe3e5ca65f401cc508be3207040
Georg Semmler <georg_semmler_05@web.de>
2014-12-04 10:37:07 +0100

    Improve add user script

--- add_user.sh
+++ add_user.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
-TASKD=../../src/taskd
-ROOT=$PWD/root
-ROOT_CA=../../pki/ca.cert.pem
+if [ -z "$1" ]
+  then
+    TASKD=../../src/taskd
+else 
+	TASKD=$1
+fi
+
+if [ -z "$2" ]
+  then
+    ROOT=$PWD/root
+else 
+	ROOT=$2
+fi
+
+if [ -z "$3" ]
+  then
+    ROOT_CA=../../pki/ca.cert.pem
+else 
+	ROOT_CA=$3
+fi
+
+if ! [ -z "$4" ] ; then
+	if [ "$4" -ge 2048 ] ; then
+		BITS=$4
+	fi
+fi
+
 
 #read username and org from comandline
 read -p "Username?`echo $'\n> '`" USER
-read -p "Org?`echo $'\n> '`" ORG
+read -p "Org?`echo $'\n> '`" ORGANIZATION
 
 #create org if nessersary
 $TASKD add --data $ROOT org $ORG >&2>/dev/null
 
 #create user
-$TASKD add --data $ROOT user --quiet $ORG $USER 1> user.key
+$TASKD add --data $ROOT user --quiet $ORGANIZATION $USER 1> user.key
 
 #find configs
 $TASKD config --data $ROOT |grep  '^server ' >server
 
-(cd ../../pki && ./generate.client $ORG$USER)
+./generate.client $ORGANIZATION$USER
 cd $PWD
-cp ../../pki/$ORG$USER.cert.pem $USER.cert
+mv $ORGANIZATION$USER.cert.pem $USER.cert
 #cat `$TASKD config --data $ROOT |grep  '^client.cert '| sed -e 's/client.cert//'`>$USER.cert
-cat ../../pki/$ORG$USER.key.pem |sed -n '/-----BEGIN RSA PRIVATE KEY-----/,/-----END RSA PRIVATE KEY-----/p' >$USER.key
+cat $ORGANIZATION$USER.key.pem |sed -n '/-----BEGIN RSA PRIVATE KEY-----/,/-----END RSA PRIVATE KEY-----/p' >$USER.key
 
 #if user-config already exists remove it
 rm -rf $USER.taskdconfig
@@ -40,10 +64,9 @@ cat $ROOT_CA>>$USER.taskdconfig
 #remove temp-files
 rm -rf user.key server $USER.cert
 rm -rf user.key server $USER.key
-#rm -f ../../pki/$ORG$USER.cert.pem
 
 
 echo 
 echo "You're ready!"
-echo "Copy the "$USER.taskdconfig" to your device and don't forget to start the server:"
-echo "./run"
+echo "Copy the "$USER.taskdconfig" to your device and don't forget to start the server"
+#echo "./run"
